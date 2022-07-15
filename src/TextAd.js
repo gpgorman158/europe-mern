24c3eb5dcbac9d0255a0df9cd99a505a67e83501
Gerald Gorman <gpgorman158@gmail.com>
2022-07-14 20:52:18 -0400

    script adjustments

--- src/TextAd.js
+++ src/TextAd.js
@@ -3,22 +3,22 @@ import { useParams, useLocation, useMatch } from 'react-router-dom';
 
 function TextAd ( {results, city}) {
 
-    let pageOptions = {
-        "pubId": "partner-pub-1187797442630842", // Make sure this is the correct client ID!
-        "query": `${results}`, // Make sure the correct query is placed here!
-        "styleId": "5769253257",
-        "adsafe": "high"
-      };
+    // let pageOptions = {
+    //     "pubId": "partner-pub-1187797442630842", // Make sure this is the correct client ID!
+    //     "query": `${results}`, // Make sure the correct query is placed here!
+    //     "styleId": "5769253257",
+    //     "adsafe": "high"
+    //   };
     
-    let adblock1 = {
-        "container": "afscontainer1"
-    }; 
+    // let adblock1 = {
+    //     "container": "afscontainer1"
+    // }; 
     // _googCsa('ads', pageOptions, adblock1);
 
   return (
     <div className="text-ad">
         {/* This is a {results} {city ? city : null} text ad page <br></br> */}
-        <div id="afscontainer1">
+        <div id="afscontainer2">
             {/* {pageOptions.query} */}Content Coming Soon!
         </div>
     </div>
