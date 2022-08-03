2525cacb304bb3169d4c0f3adbc41849917a1e5f
KingStar1001 <goldlion0819@gmail.com>
2022-08-03 11:40:36 -0300

    Update TextAd.js

--- src/TextAd.js
+++ src/TextAd.js
@@ -3,17 +3,17 @@ import { useParams, useLocation, useMatch } from 'react-router-dom';
 
 function TextAd ( {results, city}) {
 
-    // let pageOptions = {
-    //     "pubId": "partner-pub-1187797442630842", // Make sure this is the correct client ID!
-    //     "query": `${results}`, // Make sure the correct query is placed here!
-    //     "styleId": "5769253257",
-    //     "adsafe": "high"
-    //   };
+    let pageOptions = {
+        "pubId": "partner-pub-1187797442630842", // Make sure this is the correct client ID!
+        "query": `${results}`, // Make sure the correct query is placed here!
+        "styleId": "5769253257",
+        "adsafe": "high"
+      };
     
-    // let adblock1 = {
-    //     "container": "afscontainer1"
-    // }; 
-    // _googCsa('ads', pageOptions, adblock1);
+    let adblock1 = {
+        "container": "afscontainer1"
+    }; 
+    window._googCsa('ads', pageOptions, adblock1);
 
   return (
     <>
